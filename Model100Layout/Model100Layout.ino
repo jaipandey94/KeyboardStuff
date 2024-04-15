855ebdf336f6f9a32ad2da71c398af7041a4ae4a
jaipandey94 <jai.pandey94@live.com>
2024-04-15 11:03:38 -0700

    Adding more Keymaps to go between layers like the Advantage 360 Layout

--- Model100Layout/Model100Layout.ino
+++ Model100Layout/Model100Layout.ino
@@ -182,16 +182,16 @@ KEYMAPS(
 	[PRIMARY] = KEYMAP_STACKED
 	(Key_Esc,				Key_1,            		Key_2,          		Key_3,          			Key_4,          		Key_5,          Key_LeftGui,
 	 Key_Backtick,   		Key_Q,            		Key_W,          		Key_E,          			Key_R,          		Key_T,          Key_Tab,
-	 LSHIFT(Key_Minus),   	Key_A,            		Key_S,          		Key_D,          			Key_F,          		Key_G,
+	 Key_Esc,   	Key_A,            		Key_S,          		Key_D,          			Key_F,          		Key_G,
 	 Key_LeftAlt,    		Key_Z,            		Key_X,          		Key_C,          			Key_V,          		Key_B,          Key_Escape,
-	 Key_LeftControl,		Key_Backspace,    		Key_Backspace,  		Key_LeftShift,
+	 Key_LeftControl,		Key_Backspace,    		ShiftToLayer(SYMBOL),  		Key_LeftShift,
 	 ShiftToLayer(SYMBOL),
 
 	 Key_RightGui,    		Key_6,            		Key_7,          		Key_8,          			Key_9,          		Key_0,         LockLayer(NUMPAD),
 	 Key_Enter,       		Key_Y,            		Key_U,          		Key_I,          			Key_O,          		Key_P,         Key_Equals,
 							          Key_H,            		Key_J,          		Key_K,         	 			Key_L,          		Key_Semicolon, Key_Quote,
 	 Key_RightAlt,    		Key_N,            		Key_M,          		Key_Comma,      			Key_Period,     		Key_Slash,     Key_Minus,
-	 Key_RightShift,  		Key_Spacebar,     		Key_Spacebar,   		Key_RightControl,
+	 Key_RightShift,  		ShiftToLayer(NAVIGATION),     		Key_Spacebar,   		Key_RightControl,
 	 ShiftToLayer(NAVIGATION)),
 
 
